93a046460d6cd880a5f83f918dbaea8cc4622c8b
Lakshmi Gireesh <lakshmigireesh1289@gmail.com>
2025-11-25 10:55:09 +0530

    Commit

---
+++ greetings/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core.validators import validate_email
+from django.core.validators import ValidationError
+from .models import Customer
+
+
+def validate_not_gmail(value):
+    if value.find('@gmail')!=-1:
+        raise ValidationError(
+            "Gmail is not allowed",
+            params={'value':value},
+        )
+
+
+class LoginForm(forms.Form):
+    email=forms.EmailField(max_length=18,min_length=8,validators=[validate_email])
+    password=forms.CharField(max_length=18,min_length=8,widget=forms.PasswordInput)
+
+
+class LoginModelForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['email', 'password']
+
+
+
+ 
